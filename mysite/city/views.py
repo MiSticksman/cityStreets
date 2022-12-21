442bc20d76c3052396fc1e9c6a27bad4cf201e02
MiSticksman <vadim02101@gmail.com>
2022-12-22 02:34:32 +0300

    add pagination class

--- mysite/city/views.py
+++ mysite/city/views.py
@@ -9,7 +9,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Street
 from .serializers import *
 from django.shortcuts import render
-
+from rest_framework.pagination import PageNumberPagination
 from rest_framework import viewsets
 
 from .serializers import StreetSerializer
@@ -17,26 +17,60 @@ from .serializers import StreetSerializer
 from .models import Street, House, Route, RouteComponents
 
 
+class StreetViewPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
+    def paginate_queryset(self, queryset, request, view=None):
+        if 'page' not in request.query_params:
+            return None
+        return super().paginate_queryset(queryset, request, view)
 
 class StreetView(viewsets.ModelViewSet):
     serializer_class = StreetSerializer
     queryset = Street.objects.all().order_by('street_name')
+    pagination_class = StreetViewPagination
 
+
+class HouseViewPagination(PageNumberPagination):
+    page_size = 15
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+    def paginate_queryset(self, queryset, request, view=None):
+        if 'page' not in request.query_params:
+            return None
+        return super().paginate_queryset(queryset, request, view)
 
 class HouseView(viewsets.ModelViewSet):
     serializer_class = HouseSerializer
+    # queryStreetSet = Street.objects.all().order_by('street_name')
     queryset = House.objects.all().order_by('house_number')
+    pagination_class = HouseViewPagination
 
+    
 
     # def get_serializer_context(self):
     #     return {
     #         'street_id': self.request.street.street_id
     #     }
 
+
+class RouteViewPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+    def paginate_queryset(self, queryset, request, view=None):
+        if 'page' not in request.query_params:
+            return None
+        return super().paginate_queryset(queryset, request, view)
+
 class RouteView(viewsets.ModelViewSet):
     serializer_class = RouteSerializer
     queryset = Route.objects.all().order_by('route_name')
+    pagination_class = RouteViewPagination
 
 
 class RouteComponentsView(viewsets.ModelViewSet):
